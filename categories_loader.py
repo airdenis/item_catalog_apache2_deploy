ad0eb9e97ca48446f34e0828db894c2d94c3a265
Denis <air_denis@yahoo.com>
2018-12-09 18:02:21 -0800

    feat: add import psycopg2

--- categories_loader.py
+++ categories_loader.py
@@ -1,6 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, User, Category, Item
+import psycopg2
 
 def connect():
 	return psycopg2.connect("dbname=itemcatalog user=itemcatalog password=test host=localhost")
